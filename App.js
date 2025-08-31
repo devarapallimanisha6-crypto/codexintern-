02ca5e25d78bcd0e8cb4088490c71c8cc2eff84b
devarapallimanisha6-crypto <devarapallimanisha6@gmail.com>
2025-08-31 10:35:41 +0530

    Add files via upload
    task 3

--- App.js
+++ App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Translator from "./pages/Translator";
-import RandomStringGenerator from "./pages/RandomStringGenerator";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-gray-100">
-        <nav className="bg-blue-600 text-white p-4 flex gap-4">
-          <Link to="/" className="hover:underline">Translator</Link>
-          <Link to="/random" className="hover:underline">Random String Generator</Link>
-        </nav>
-        <main className="p-4">
-          <Routes>
-            <Route path="/" element={<Translator />} />
-            <Route path="/random" element={<RandomStringGenerator />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
